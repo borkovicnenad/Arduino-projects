7da17cc606f68c12b774864c9579fa6c708d82fe
Nenad Borkovic <borkovic.nenad25@gmail.com>
2025-08-17 17:19:49 +0200

    add: Love-o-meter

---
+++ 3_Love-o-meter/src/main.cpp
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+
+const int tempSensorPin = A0;
+const float baselineTemp = 35.0;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  for (int i = 0; i < 5; i++)
+  {
+    for (int pinNo = 3; pinNo <= 5; pinNo++)
+    {
+      pinMode(pinNo, OUTPUT);
+      digitalWrite(pinNo, HIGH);
+      delay(100);
+      digitalWrite(pinNo, LOW);
+    }
+  }
+}
+
+void loop()
+{
+  int tempSensorVal = analogRead(tempSensorPin);
+  delay(2000);
+  float voltage = tempSensorVal * (5.0 / 1023.0);
+  float temperature = (voltage - 0.5f) * 100.0f; // MCP9700: 10 mV/°C, 0.5 V @ 0°C
+
+  Serial.print("Sensor value: ");
+  Serial.println(tempSensorVal);
+  Serial.print("Voltage: ");
+  Serial.println(voltage, 3);
+  Serial.print("Temperature: ");
+  Serial.println(temperature, 1);
+  Serial.println();
+
+
+  if (temperature < 34)
+  {
+
+    digitalWrite(3, HIGH);
+    delay(200);
+    digitalWrite(3, LOW);
+  }
+  else if (temperature > 36)
+  {
+
+    digitalWrite(5, HIGH);
+    delay(200);
+    digitalWrite(5, LOW);
+  }
+  else
+  {
+
+    digitalWrite(4, HIGH);
+    delay(200);
+    digitalWrite(4, LOW);
+  }
+}

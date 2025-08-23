96d2058d7abb8a33728be51e5d7385c27e4e352c
Nenad Borkovic <borkovic.nenad25@gmail.com>
2025-08-23 17:33:39 +0200

    Keyboard instrument

---
+++ 7_Keyboard_Instrument/src/main.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+
+int notes[] = {262, 294, 330, 349};
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int keyValue = analogRead(A0);
+  Serial.println(keyValue);
+
+  if (keyValue == 1023)
+  {
+    tone(8, notes[0]);
+  }
+  else if (keyValue >= 1000 && keyValue <= 1010)
+  {
+    tone(8, notes[1]);
+  }
+  else if (keyValue >= 930 && keyValue <= 940)
+  {
+    tone(8, notes[2]);
+  }
+  else if (keyValue >= 505 && keyValue <= 515)
+  {
+    tone(8, notes[3]);
+  }
+  else if (keyValue >= 5 && keyValue <= 10)
+  {
+    tone(8, notes[3]);
+  }
+  else
+  {
+    noTone(8);
+  }
+}
